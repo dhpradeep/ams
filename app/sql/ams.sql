b1720aaa53ed50d85a85297b3900c2b3794b8423
dhpradeep <dhpradeep25@gmail.com>
2019-08-19 02:52:53 +0545

    Database aded

--- app/sql/ams.sql
+++ app/sql/ams.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 13, 2019 at 05:28 PM
+-- Generation Time: Aug 18, 2019 at 11:07 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.1.30
 
@@ -25,11 +25,137 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `subjectId` int(11) NOT NULL,
+  `date` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactdetails`
+--
+
+CREATE TABLE `contactdetails` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `municipality` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `wardNo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `area` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `district` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `zone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `mobileNo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `telephoneNo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `blockNo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `guardianName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `guardianRelation` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `guardianContact` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documents`
+--
+
+CREATE TABLE `documents` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `formNo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `entranceNo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `eligible` varchar(5) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'false',
+  `remarks` varchar(1200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `marksheet_see` varchar(5) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'false',
+  `marksheet_11` varchar(5) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'false',
+  `marksheet_12` varchar(5) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'false',
+  `transcript` varchar(5) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'false',
+  `characterCertificate_see` varchar(5) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'false',
+  `characterCertificate_12` varchar(5) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'false',
+  `citizenship` varchar(5) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'false',
+  `photo` varchar(5) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'false'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `education`
+--
+
+CREATE TABLE `education` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `level` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `faculty` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `institution` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `board` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `yearOfCompletion` int(11) NOT NULL,
+  `percent` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personaldata`
+--
+
+CREATE TABLE `personaldata` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `programId` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `doa` date NOT NULL,
+  `dobAd` date NOT NULL,
+  `dobBs` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `gender` tinyint(11) NOT NULL,
+  `nationality` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Nepali',
+  `fatherName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `program`
+--
+
+CREATE TABLE `program` (
+  `id` int(11) NOT NULL,
+  `programId` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `details` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `yearOrSemester` tinyint(1) NOT NULL,
+  `noOfYearOrSemester` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `programId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `semesterOrYear` tinyint(4) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `creditHr` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `details` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `userlogin`
 --
 
 CREATE TABLE `userlogin` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL COMMENT 'userID for others',
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `fname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `mname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -40,15 +166,50 @@ CREATE TABLE `userlogin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `userlogin`
---
-
-INSERT INTO `userlogin` (`id`, `username`, `fname`, `mname`, `lname`, `email`, `passwordHash`, `role`) VALUES
-(1, 'admin', 'pradip', NULL, 'dhakal', 'admin@admin.com', 'e00cf25ad42683b3df678c61f42c6bda', 1);
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contactdetails`
+--
+ALTER TABLE `contactdetails`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `education`
+--
+ALTER TABLE `education`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `personaldata`
+--
+ALTER TABLE `personaldata`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `program`
+--
+ALTER TABLE `program`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `userlogin`
@@ -61,10 +222,52 @@ ALTER TABLE `userlogin`
 --
 
 --
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `contactdetails`
+--
+ALTER TABLE `contactdetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `education`
+--
+ALTER TABLE `education`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `personaldata`
+--
+ALTER TABLE `personaldata`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `program`
+--
+ALTER TABLE `program`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `userlogin`
 --
 ALTER TABLE `userlogin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'userID for others', AUTO_INCREMENT=60;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
