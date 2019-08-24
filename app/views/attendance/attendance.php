df507f21bfe63eba24a75017fc8da50c2a6d92f5
Raju Lamsal <rajulamsal62@gmail.com>
2019-08-24 19:35:27 +0545

    attendance frontend completed

--- app/views/attendance/attendance.php
+++ app/views/attendance/attendance.php
@@ -21,108 +21,138 @@
     <!-- Custom Fonts -->
     <link href="<?= BOWER_DIR ?>/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
-
-<div id="wrapper">
-
-<!-- Navigation -->
-<?php include(INCLUDES_DIR.DS.'nav-bar.php'); ?>
-
-<div id="page-wrapper">
-    <div class="row">
-        <div class="col-lg-12">
-            <h2 class="page-header text-primary">Attendance</h2>
-            <ol class="breadcrumb">
-                <li>
-                    <i class="fa fa-dashboard"></i>
-                    <a href="<?= SITE_URL.DS.'home'.DS ?>dashboard">Dashboard</a>
-                </li>
-                <li class="active">
-                    Attendance
-                </li>
-            </ol>
+  <div id="wrapper">
+    <!-- Navigation -->
+    <?php include(INCLUDES_DIR.DS.'nav-bar.php'); ?>
+    <!-- Page Wrapper -->
+    <div id="page-wrapper">
+      <div class="container-fluid">
+        <div class="row" style="margin:0 auto;">
+          <div class="col-md-6">
+            <form role="form" class="form-group">
+              <label for="currentdate">Current Date</label>
+              <input type="date" id="currentdate" name="currentdate"  class="form-control"/>
+            </form>
+          </div>
+          <div class="col-md-6"></div>
         </div>
-        <!-- /.col-lg-12 -->
-    </div>
-    <!-- /.row -->
-    <div class="row">
-        <div class="col-lg-12">
-            <div class=" form-padding">
-                <form id="frmSearch" role="form" class="form-inline">
-                    <input type="date" name="currentdate" id="currentdate" class="form-control"/>
-                    <select name="course" id="course" class="form-control">
-                        <option value="select">Choose Program</option>
-                        <option value="bca">BCA</option>
-                        <option value="bph">BPH</option>
-                        <option value="bba">BBA</option>
-                    </select>
-                    <select name="subject" id="subject" class="form-control">
-                        <option value="type">Choose Subject</option>
-                        <option value="dsa">DSA</option>
-                        <option value="php">PHP</option>
-                    </select>
-                </form>
-            </div>
-            <br>
-            <!-- <div class="panel panel-primary">
-                <div class="panel-heading">
-                    List of Students
-                </div>
-                <div class="panel-body"> -->
-                  <h3 class="text-center text-primary">List of Students</h3>
-                    <div class="dataTable_wrapper attTable">
-                      <table class="table">
-                        <thead>
-                          <tr>
-                            <th scope="col">Roll No</th>
-                            <th scope="col">Full Name</th>
-                            <th scope="col">Status</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>1</td>
-                            <td>Raju Lamsal</td>
-                            <td>
-                              <label class="switch">
-                                <input type="checkbox">
-                                <span class="slider round"></span>
-                              </label>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>2</td>
-                            <td>Pradip Dhakal</td>
-                            <td>
-                              <label class="switch">
-                                <input type="checkbox">
-                                <span class="slider round"></span>
-                              </label>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>3</td>
-                            <td>Arzun Subedi</td>
-                            <td>
-                              <label class="switch">
-                                <input type="checkbox">
-                                <span class="slider round"></span>
-                              </label>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                      <button type="submit" class="btn btn-primary pull-right">Submit</button>
-                    </div>
-                </div>
-            </div>
+        <!-- End First Row -->
+        <!-- start 2nd row -->
+        <div class="row" style="margin:0 auto;">
+          <div class="col-md-6">
+              <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                  <div class="panel panel-default">
+                      <div class="panel-heading" role="tab" id="headingOne">
+                          <h4 class="panel-title">
+                              <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                  <i class="more-less glyphicon glyphicon-plus"></i>
+                                  Collapsible Group Item #1
+                              </a>
+                          </h4>
+                      </div>
+                      <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                          <div class="panel-body">
+                                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+                                <a href="#" class="btn btn-info text-center" role="button">Select This</a>
+                          </div>
+                      </div>
+                  </div> 
+                  <div class="panel panel-default">
+                      <div class="panel-heading" role="tab" id="headingOne">
+                          <h4 class="panel-title">
+                              <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                  <i class="more-less glyphicon glyphicon-plus"></i>
+                                  Collapsible Group Item #2
+                              </a>
+                          </h4>
+                      </div>
+                      <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                          <div class="panel-body">
+                                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+                                <a href="#" class="btn btn-info text-center" role="button">Select This</a>
+                          </div>
+                      </div>
+                  </div> 
+                  <div class="panel panel-default">
+                      <div class="panel-heading" role="tab" id="headingOne">
+                          <h4 class="panel-title">
+                              <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                  <i class="more-less glyphicon glyphicon-plus"></i>
+                                  Collapsible Group Item #3
+                              </a>
+                          </h4>
+                      </div>
+                      <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                          <div class="panel-body">
+                                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+                                <a href="#" class="btn btn-info text-center" role="button">Select This</a>
+                          </div>
+                      </div>
+                  </div>      
+              </div>
+          </div>
+          <div class="col-md-6"></div>
         </div>
+        <!-- End 2nd Row -->
+        <div class="row">
+          <div class="col-md-6">
+              <h3 class="text-center text-primary">List of Students</h3>                  <div class="dataTable_wrapper attTable">
+                  <table class="table attTable">
+                    <thead>
+                      <tr>
+                        <th scope="col">Roll No</th>
+                        <th scope="col">Full Name</th>
+                        <th scope="col">
+                          <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span>
+                          </label>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td>Raju Lamsal</td>
+                        <td>
+                          <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span>
+                          </label>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td>Pradip Dhakal</td>
+                        <td>
+                          <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span>
+                          </label>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>3</td>
+                        <td>Arzun Subedi</td>
+                        <td>
+                          <label class="switch">
+                            <input type="checkbox">
+                            <span class="slider round"></span>
+                          </label>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <button type="submit" class="btn btn-primary pull-left">Submit</button>
+                </div>
+          </div>
+          <div class="col-md-6"></div>
+        </div>
+      </div>
     </div>
-<!-- /#wrapper ->
+  </div>
 
-            <!-- modals -->
-            <?php include(MODALS_DIR.DS.'course.php'); ?>
      <!-- jQuery -->
      <script src="<?= BOWER_DIR ?>/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap Core JavaScript -->
